896c13dcdcdae697662a42bd8701755a7dadbcb1
muskannadaf <mnadafcisco@ip-172-31-23-125.ec2.internal>
2024-02-29 07:03:55 +0000

    First Commit

---
+++ src/main/java/com/cisco/SimplemavenProject/App.java
@@ -0,0 +1,14 @@
+package com.cisco.SimplemavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+}
